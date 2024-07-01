4a27945c750b771b65b7696f18fe7b4e708e3e89
ahmed-abualsaud <ahmed.m.abualsaud@gmail.com>
2024-07-01 21:29:13 +0300

    remove legacy code

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Epush\Mail\Infra\Mail\ClientAdded;
-use Epush\Mail\Infra\Mail\OrderAdded;
+// use Epush\Mail\Infra\Mail\ClientAdded;
+// use Epush\Mail\Infra\Mail\OrderAdded;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return (new OrderAdded(['credit' => '100', 'price' => '0.15', 'balance' => '10020', 'messages_count' => '11434']));
-    return (new ClientAdded(['username' => 'ahmed', 'phone' => '01126999840']));
+    // return (new OrderAdded(['credit' => '100', 'price' => '0.15', 'balance' => '10020', 'messages_count' => '11434']));
+    // return (new ClientAdded(['username' => 'ahmed', 'phone' => '01126999840']));
 
     return view('welcome');
 });
